4bcff0de6551083bc5574fb1552e3f792e832d4a
Mr-angelo <angelcarrillo107@gmail.com>
2021-01-08 04:14:39 -0600

    Cuarto commit

--- src/test/PersonaPrueba.java
+++ src/test/PersonaPrueba.java
@@ -6,7 +6,11 @@ public class PersonaPrueba {
     public static void main(String[] args) {
         
         Persona persona1 = new Persona("Firulais");
+        Persona persona2 = new Persona("Willilis");
+        
+        
         System.out.println("Persona1 = " + persona1 );
+         System.out.println("Persona2 = " + persona2 );
         
     }
     

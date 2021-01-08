4fbf75e6d3b10b1fe5ae584409103e53824ceaed
Mr-angelo <angelcarrillo107@gmail.com>
2021-01-08 03:45:49 -0600

    Segundo commit

--- src/domain/Persona.java
+++ src/domain/Persona.java
@@ -7,4 +7,45 @@ package domain;
  */
 public class Persona {
     
+    private int idPersona;
+    private String nombre;
+    private static int contadorPersona;
+    
+    public Persona(String nombre){
+        this.nombre = nombre;
+        //Incrementa el valor del contador
+        Persona.contadorPersona++;
+        //Asingnar valor al id
+        this.idPersona= contadorPersona;
+        
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+        public static int getContadorPersona() {
+        return contadorPersona;
+    }
+
+    public static void setContadorPersona(int aContadorPersona) {
+        contadorPersona = aContadorPersona;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + ", nombre=" + nombre + '}';
+    }
 }
